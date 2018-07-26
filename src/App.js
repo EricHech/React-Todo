5b12b1c2e8720d4e8156879b85e04e47502d481d
Eric <erichech@gmail.com>
2018-07-26 14:32:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import uuid from "uuid";
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        task: 'Organize Garage',
+        id: uuid(),
+        completed: false
+      },
+      {
+        task: 'Bake Cookies',
+        id: uuid(),
+        completed: false
+      },
+    ],
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
